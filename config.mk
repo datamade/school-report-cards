d1ab863b604a866399786b45564ce6d740d05ef2
Forest Gregg <fgregg@uchicago.edu>
2015-12-30 16:05:18 -0600

    config mk

---
+++ config.mk
@@ -0,0 +1,12 @@
+MAKEFLAGS += --warn-undefined-variables
+SHELL := bash
+.SHELLFLAGS := -eu -o pipefail
+.DEFAULT_GOAL := all
+.DELETE_ON_ERROR:
+.SUFFIXES:
+
+PG_HOST="localhost"
+PG_USER="fgregg"
+PG_DB="schools"
+PG_PORT="5432"
+PG_PASS="buddah"
