bdb756b62b0273790c6bb2e849b4f77d394bd5f6
Forest Gregg <fgregg@uchicago.edu>
2016-05-25 21:59:04 -0500

    xlsx not xlsx.py

--- csv.mk
+++ csv.mk
@@ -62,7 +62,7 @@ RC15_layout.xlsx :
 .INTERMEDIATE: RC13_layout.csv RC14_layout.csv RC15_layout.csv		\
                RC13_layout.xlsx RC14_layout.xlsx RC15_layout.xlsx
 RC13_layout.csv RC14_layout.csv RC15_layout.csv : %.csv : %.xlsx
-	xlsx2csv.py $< > $@
+	xlsx2csv $< > $@
 
 schema_19%.csv schema_20%.csv: RC%_layout.csv
 	iconv -f WINDOWS-1252 -t UTF8 $< | python schema.py $($*_col) | python normalize_schema.py > $@
