e9ffec9fdd5c75098d78d0b6fc5e51fb057a3b5c
Forest Gregg <fgregg@uchicago.edu>
2016-10-12 23:05:33 -0500

    all types of school districts

--- layers.mk
+++ layers.mk
@@ -5,12 +5,27 @@ postgis:
 tl_2016_17_unsd.zip :
 	wget -O $@ 'ftp://ftp2.census.gov/geo/tiger/TIGER2016/UNSD/tl_2016_17_unsd.zip'
 
+tl_2016_17_elsd.zip :
+	wget -O $@ 'ftp://ftp2.census.gov/geo/tiger/TIGER2016/ELSD/tl_2016_17_elsd.zip'
+
+tl_2016_17_scsd.zip :
+	wget -O $@ 'ftp://ftp2.census.gov/geo/tiger/TIGER2016/SCSD/tl_2016_17_scsd.zip'
+
+
 # unzip shapefiles
-tl_2016_17_unsd.shp : tl_2016_17_unsd.zip
+tl_2016_17_%.shp : tl_2016_17_%.zip
 	unzip $^
 	touch $@
 
 # import district shapefile to the school report card DB
-district_boundaries : tl_2016_17_unsd.shp postgis
-	shp2pgsql -s 4326 $< $@ | psql -d $(PG_DB)
+%_boundaries : tl_2016_17_%.shp postgis
+	shp2pgsql -s 4326 -d $< $@ | psql -d $(PG_DB)
 	touch $@
+
+district_boundaries : unsd_boundaries elsd_boundaries scsd_boundaries
+	psql -d $(PG_DB) -c "CREATE TABLE $@ AS \
+                             (SELECT * FROM $< \
+                              UNION \
+                              SELECT * FROM $(word 2,$^) \
+                              UNION \
+                              SELECT * FROM $(word 3,$^))"
